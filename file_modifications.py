c30dc2015216e260ef0f23983c1ef67f66213f54
JeffTheNinja57 <mihny.angheluta@gmail.com>
2024-05-07 12:47:36 +0200

    add session naming edits

--- file_modifications.py
+++ file_modifications.py
@@ -21,6 +21,25 @@ def remove_zero_from_run(filepath):
         print(f"Renamed: {filepath} -> {new_filepath}")
 
 
+def remove_zero_from_session(filepath):
+    # Extract the directory path and filename from the filepath
+    directory, filename = os.path.split(filepath)
+
+    # Define the pattern to match 'ses-' followed by one or two digits before the file extension
+    pattern = r'(ses-)(\d{1,2})(_)$'
+
+    # Use re.sub to replace the matched session number pattern in the filename
+    modified_filename = re.sub(pattern, lambda x: x.group(1) + str(int(x.group(2))) + x.group(3), filename)
+
+    # Construct the new full path with the modified filename
+    new_filepath = os.path.join(directory, modified_filename)
+
+    # Rename the file if the new path is different
+    if filepath != new_filepath:
+        os.rename(filepath, new_filepath)
+        print(f"Renamed: {filepath} -> {new_filepath}")
+
+
 def process_directory(directory):
     # Traverse through all files and directories in the specified directory
     for root, dirs, files in os.walk(directory):
@@ -29,18 +48,20 @@ def process_directory(directory):
                 # Construct the full path of the file
                 filepath = os.path.join(root, filename)
 
-                # Process the file to remove leading zeros from run numbers
+                # Process the file to remove leading zeros from run and session numbers
                 remove_zero_from_run(filepath)
+                remove_zero_from_session(filepath)
 
 
-# Specify the top-level directory to process
-top_directory = "/Users/mihnea/_workspace_/_uni/workshop/fmri_data/images"
+if __name__ == "__main__":
+    # Specify the top-level directory to process
+    top_directory = "/Users/mihnea/_workspace_/_uni/workshop/fmri_data/images"
 
-# Process all subdirectories within the top-level directory
-for root, dirs, files in os.walk(top_directory):
-    for directory in dirs:
-        dir_path = os.path.join(root, directory)
-        process_directory(dir_path)
+    # Process all subdirectories within the top-level directory
+    for root, dirs, files in os.walk(top_directory):
+        for directory in dirs:
+            dir_path = os.path.join(root, directory)
+            process_directory(dir_path)
 
-# Also process files directly under the top-level directory
-process_directory(top_directory)
+    # Also process files directly under the top-level directory
+    process_directory(top_directory)
